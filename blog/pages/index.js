dc7bbeaa44c40e0b148e4b5e2fcfaa3fafbee6b4
nikosantis <nikosantis@gmail.com>
2021-04-20 17:43:12 -0400

    update for deploy

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -20,7 +20,7 @@ export default function Blog() {
       <a href="/">Home</a>
       <div>
         <Image
-          src="/blog/static/nextjs.png"
+          src="/blog/nextjs.png"
           alt="Next.js logo"
           width={200}
           height={160}
